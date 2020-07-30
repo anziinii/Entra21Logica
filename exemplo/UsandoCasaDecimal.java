396dfd3280b3965bda31092a5ba572b81731ad53
Anziinii <felipe_anzini@hotmail.com>
2020-07-30 20:45:44 -0300

    Usando casa decimal

---
+++ exemplo/UsandoCasaDecimal.java
@@ -0,0 +1,11 @@
+public class UsandoCasaDecimal{
+    public static void main(String[] args){
+            String nome;
+            double altura;
+            
+            nome = "Felipe";
+            altura = 1.84;
+
+        System.out.println("Ola meu nome e " + nome + " e tenho " + altura + " de altura");
+    }
+    }
