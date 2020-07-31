2a51b6a2b6ef6761138b19f63d4df6684ee44289
Anziinii <felipe_anzini@hotmail.com>
2020-07-30 21:00:03 -0300

    adicionando soma e subtracao

--- exemplo/CalculaValores.java
+++ exemplo/CalculaValores.java
@@ -1,4 +1,18 @@
 public class CalculaValores{
     public static void main(String[] args){
-            }
+        
+    int nummeroUm;
+    int nummeroDois;
+    int soma, subtracao;
+
+    nummeroUm = 10;
+    nummeroDois = 50;
+
+    soma = nummeroUm + nummeroDois;
+    subtracao = nummeroUm - nummeroDois;
+
+    System.out.println("O resultado da soma: " + soma);
+    System.out.println("O resultado da soma: " + subtracao);
+    
+    }
     }
