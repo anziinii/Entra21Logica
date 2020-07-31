5a1c84b7f707d30ecb113c26004e2a42678fc0c6
Anziinii <felipe_anzini@hotmail.com>
2020-07-30 21:08:13 -0300

    aadicionando multiplicacao

--- exemplo/CalculaValores.java
+++ exemplo/CalculaValores.java
@@ -3,18 +3,19 @@ public class CalculaValores{
         
     double nummeroUm;
     double nummeroDois;
-    double soma, subtracao;
-    double divisao;
-
+    double soma, subtracao, divisao, multiplicacao;
+    
     nummeroUm = 10;
     nummeroDois = 50;
 
     soma = nummeroUm + nummeroDois;
     subtracao = nummeroUm - nummeroDois;
     divisao = nummeroUm / nummeroDois;
+    multiplicacao = nummeroUm * nummeroDois;
 
     System.out.println("O resultado da soma: " + soma);
     System.out.println("O resultado da soma: " + subtracao);
     System.out.println("O resultado da soma: " + divisao);
+    System.out.println("O resultado da soma: " + multiplicacao);
     }
     }
