cb19cfac766674d9763ff1a03d32c3dae29299fe
Anziinii <felipe_anzini@hotmail.com>
2020-08-03 21:19:16 -0300

    Calculadora altomatica

---
+++ exemplo/Calculadora.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Calculadora{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro valor");
+        double num1 = sc.nextDouble();
+     
+        System.out.println("Digite o segundo valor");
+        double num2 = sc.nextDouble();
+    
+        double soma = num1 + num2;
+        double subtracao = num1 - num2;
+        double divisao = num1 / num2;
+        double multiplicacao = num1 * num2;
+       
+        System.out.println("O resultado da soma dos valores e: " + soma);
+
+        System.out.println("O resultado da subtracao dos valores e: " + subtracao);
+
+        System.out.println("O resultado da divisao dos valores e: " + divisao);
+
+        System.out.println("O resultado da multiplicacao dos valores e: " + multiplicacao);
+    
+
+        sc.close();
+
+    }
+    }
